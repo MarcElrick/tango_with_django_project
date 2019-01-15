fe9956d2d74c6c60f09e0396e6a67bea2492fbee
Marc Elrick <2316291e@student.gla.ac.uk>
2019-01-15 14:02:35 +0000

    Went back and completed some chapter 3 exercises which were skipped over earlier.

--- rango/views.py
+++ rango/views.py
@@ -6,5 +6,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {}
-    return render(request, 'rango/about.html', context=context_dict)
+    return HttpResponse('Rango Says here is the about page!')
