43541572dc0c0918aa98de91fb7d7d9a71004d9c
Marc Elrick <2316291e@student.gla.ac.uk>
2019-01-15 14:23:28 +0000

    Added an image of a cat to the about page, hereby completing chapter 4.

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
 ]
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
@@ -126,4 +128,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+
