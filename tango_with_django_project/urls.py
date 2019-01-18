0eaaab7c06977af18b8a1f5e98e7c4cc44889ff1
Marc Elrick <2316291e@student.gla.ac.uk>
2019-01-18 21:10:34 +0000

    Added the polls app to the project which was started in a different directory.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
