9e18f43983c1b7e20252af418a5a257e58c7e6de
MarcElrick <marcelrick15@hotmail.com>
2019-01-30 10:42:13 +0000

    Completed the exercises at the end of chapter 5.

--- rango/models.py
+++ rango/models.py
@@ -15,7 +15,6 @@ class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
-    views = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
